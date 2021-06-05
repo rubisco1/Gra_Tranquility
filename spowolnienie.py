154cd47dff506fcfc272cdfba1154c951a19791b
quite-bored <83338599+quite-bored@users.noreply.github.com>
2021-06-05 22:37:01 +0200

    Delete spowolnienie.py

--- spowolnienie.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-def spowolnienie():#nwm jeszcze czy dołączymy do klasy gracz, czy owoc, czy osobna funkcja
-    koniec = datetime.now() + timedelta(seconds=1)
-    while datetime.now() < koniec:#pętla będzie działać 30 sekund
-        print("1")
-
-spowolnienie()
